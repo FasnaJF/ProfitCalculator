0e33ed66ee8f0f9da1f48e6b0b9ffc3cbb87d2c0
jffasna <fasna@rhino-partners.com>
2022-10-09 17:28:21 +0530

    Updates

---
+++ precache-manifest.52ddc32822da43b3922dee486c2058c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "139ba9736d08cf490c94",
+    "url": "/ProfitCalculator/static/css/main.a2c1d58e.chunk.css"
+  },
+  {
+    "revision": "139ba9736d08cf490c94",
+    "url": "/ProfitCalculator/static/js/main.139ba973.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/ProfitCalculator/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/ProfitCalculator/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "68e02243e392924d686b",
+    "url": "/ProfitCalculator/static/js/runtime~main.68e02243.js"
+  },
+  {
+    "revision": "3113a77f011e11b0c3d4e0ccd38c59c3",
+    "url": "/ProfitCalculator/index.html"
+  }
+];
